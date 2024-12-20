dfc4bcd0d69a001d0ccec80dc05b02f2b75f35b4
diogo78oliveira <105199845+diogo78oliveira@users.noreply.github.com>
2024-12-20 16:08:07 +0000

    d

--- app/src/main/java/com/example/lojasocial_g16/FireBaseAuthenticationApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lojasocial_g16
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class FireBaseAuthenticationApp: Application() {
-}
