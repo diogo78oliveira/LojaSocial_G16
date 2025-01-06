b983ece1cf2c3d391329938f0352085438190a86
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2025-01-06 22:02:02 +0000

    style: 💄 Scheadule UI

--- app/src/main/java/com/example/g16_lojasocial/views/NotificationPage.kt
+++ app/src/main/java/com/example/g16_lojasocial/views/NotificationPage.kt
@@ -32,6 +32,8 @@ import androidx.compose.foundation.border
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.OutlinedTextField
@@ -44,6 +46,7 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -77,149 +80,207 @@ fun NotificationPage(modifier: Modifier = Modifier, isVoluntario: Boolean, views
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(Color(0xFFFFFFFF))
+            .padding(24.dp, 48.dp, 24.dp, 16.dp)
+        ,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
     ) {
         // Log to see the current value of isVoluntario and selectedDate
         Log.d("NotificationPage", "isVoluntario: $isVoluntario, selectedDate: $selectedDate")
 
-        // Display the question with the selected date if it's available
-        Text(
-            text = if (selectedDate != null) {
-                "Estás disposta a ajudar no dia $selectedDate?"
-            } else {
-                "Estás disposta a ajudar? (Data não disponível)"
-            },
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        if (isVoluntario) {
-            // LazyColumn with options "Sim" and "Não"
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
+        Card(
+            modifier = Modifier.fillMaxWidth()
+                .shadow(
+                    elevation = 20.dp,
+                    spotColor = Color(0xFF000000).copy(alpha = 0.25f),
+                    shape = RoundedCornerShape(0.dp),
+                )
+                .clip(RoundedCornerShape(5.dp))
+                .background(Color.White)
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            // Display the question with the selected date if it's available
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
             ) {
-                items(2) { index ->
-                    Row(
+                Text(
+                    text = "Estás disposta a ajudar?",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color(0xFF101214)
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = if (selectedDate != null) {
+                        "$selectedDate"
+                    } else {
+                        "(Data não disponível)"
+                    },
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color(0xFF8C98AB)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                if (isVoluntario) {
+                    // LazyColumn with options "Sim" and "Não"
+                    LazyColumn(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(vertical = 8.dp)
-                            .background(Color.White.copy(alpha = 0.1f)),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
+                            .padding(16.dp)
                     ) {
-                        Text(
-                            text = if (index == 0) "Sim" else "Não",
-                            fontSize = 16.sp,
-                            color = Color.Black
-                        )
-                        Checkbox(
-                            checked = respostaAjuda == if (index == 0) "Sim" else "Não",
-                            onCheckedChange = { isChecked ->
-                                val selectedResponse = if (index == 0) "Sim" else "Não"
-                                viewsViewModel.updateRespostaAjuda(selectedResponse) { success ->
-                                    if (success) {
-                                        respostaAjuda = selectedResponse
-                                        // Reload Voluntarios after updating respostaAjuda
-                                        viewsViewModel.loadVoluntariosSim()
-                                    } else {
-                                        Toast.makeText(context, "Erro ao salvar a resposta", Toast.LENGTH_SHORT).show()
-                                    }
-                                }
-                            },
-                            colors = CheckboxDefaults.colors(checkedColor = Color.Green)
-                        )
+                        items(2) { index ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp)
+                                    .background(Color.White.copy(alpha = 0.1f)),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = if (index == 0) "Sim" else "Não",
+                                    fontSize = 16.sp,
+                                    color = Color.Black
+                                )
+                                Checkbox(
+                                    checked = respostaAjuda == if (index == 0) "Sim" else "Não",
+                                    onCheckedChange = { isChecked ->
+                                        val selectedResponse = if (index == 0) "Sim" else "Não"
+                                        viewsViewModel.updateRespostaAjuda(selectedResponse) { success ->
+                                            if (success) {
+                                                respostaAjuda = selectedResponse
+                                                // Reload Voluntarios after updating respostaAjuda
+                                                viewsViewModel.loadVoluntariosSim()
+                                            } else {
+                                                Toast.makeText(
+                                                    context,
+                                                    "Erro ao salvar a resposta",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }
+                                        }
+                                    },
+                                    colors = CheckboxDefaults.colors(checkedColor = Color.Green)
+                                )
+                            }
+                        }
                     }
-                }
-            }
-        } else {
-            // "Alterar dia" button for non-voluntarios
-            Button(
-                onClick = {
-                    // Get the current date to set as the default date
-                    val calendar = Calendar.getInstance()
-                    val year = calendar.get(Calendar.YEAR)
-                    val month = calendar.get(Calendar.MONTH)
-                    val day = calendar.get(Calendar.DAY_OF_MONTH)
+                } else {
+                    // "Alterar dia" button for non-voluntarios
+                    Button(
+                        onClick = {
+                            // Get the current date to set as the default date
+                            val calendar = Calendar.getInstance()
+                            val year = calendar.get(Calendar.YEAR)
+                            val month = calendar.get(Calendar.MONTH)
+                            val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-                    val datePickerDialog = DatePickerDialog(
-                        context,
-                        { _, selectedYear, selectedMonth, selectedDay ->
-                            val formattedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
-                            selectedDate = formattedDate
+                            val datePickerDialog = DatePickerDialog(
+                                context,
+                                { _, selectedYear, selectedMonth, selectedDay ->
+                                    val formattedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                                    selectedDate = formattedDate
 
-                            // Log the selected date
-                            Log.d("NotificationPage", "Selected date: $formattedDate")
+                                    // Log the selected date
+                                    Log.d("NotificationPage", "Selected date: $formattedDate")
 
-                            // Save the selected date to Firestore
-                            viewsViewModel.saveDateToFirebase(formattedDate) { success ->
-                                if (success) {
-                                    // Update respostaAjuda for all users to "Não"
-                                    viewsViewModel.updateRespostaAjudaForAllUsers("Não") { updateSuccess ->
-                                        if (updateSuccess) {
-                                            Toast.makeText(context, "Data e resposta atualizadas com sucesso!", Toast.LENGTH_SHORT).show()
-                                            // Reload Voluntarios after updating
-                                            viewsViewModel.loadVoluntariosSim()
+                                    // Save the selected date to Firestore
+                                    viewsViewModel.saveDateToFirebase(formattedDate) { success ->
+                                        if (success) {
+                                            // Update respostaAjuda for all users to "Não"
+                                            viewsViewModel.updateRespostaAjudaForAllUsers("Não") { updateSuccess ->
+                                                if (updateSuccess) {
+                                                    Toast.makeText(context, "Data e resposta atualizadas com sucesso!", Toast.LENGTH_SHORT).show()
+                                                    // Reload Voluntarios after updating
+                                                    viewsViewModel.loadVoluntariosSim()
+                                                } else {
+                                                    Toast.makeText(context, "Erro ao atualizar respostas.", Toast.LENGTH_SHORT).show()
+                                                }
+                                            }
                                         } else {
-                                            Toast.makeText(context, "Erro ao atualizar respostas.", Toast.LENGTH_SHORT).show()
+                                            Toast.makeText(context, "Erro ao salvar a data.", Toast.LENGTH_SHORT).show()
                                         }
                                     }
-                                } else {
-                                    Toast.makeText(context, "Erro ao salvar a data.", Toast.LENGTH_SHORT).show()
-                                }
-                            }
+                                },
+                                year,
+                                month,
+                                day
+                            )
+                            datePickerDialog.show()
                         },
-                        year,
-                        month,
-                        day
-                    )
-                    datePickerDialog.show()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Alterar dia")
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(12.dp))
+                            .border(2.dp, Color(0xFF004EBB), RoundedCornerShape(12.dp))
+                            .background(Color(0xFF004EBB)),
+                        colors = ButtonDefaults.textButtonColors(containerColor = Color(0xFF004EBB))
+                    ) {
+                        Text("Alterar dia", color = Color(0xFFFFFFFF))
+                    }
+
+                }
             }
-
         }
 
-        // Table-like layout for Voluntarios with respostaAjuda = "Sim"
-        Spacer(modifier = Modifier.height(30.dp))
-        Text(text = "Voluntários que irão estar presentes", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(50.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-        // Table Header
-        Row(
+        Text(
+            text = "Voluntários que irão estar presentes",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF101214)
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Box(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .fillMaxWidth()
+                .shadow(
+                    elevation = 20.dp,
+                    spotColor = Color(0xFF000000).copy(alpha = 0.55f),
+                    shape = RoundedCornerShape(5.dp),
+                )
+                .clip(RoundedCornerShape(5.dp))
+                .background(Color(0xFFFFFFFF))
         ) {
-            Text("Nome", modifier = Modifier.weight(1f), fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color.Black)
-            Text("Telemóvel", modifier = Modifier.weight(1f), fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color.Black)
-        }
-
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            items(voluntariosSim) { voluntario ->
+            Column() {
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .border(1.dp, Color.LightGray),
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(voluntario.nome, modifier = Modifier.weight(1f), fontSize = 16.sp)
-                    Text(voluntario.telemovel, modifier = Modifier.weight(1f), fontSize = 16.sp)
+                    Text("Nome", modifier = Modifier.weight(1f), fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color(0xFF101214))
+                    Text("Telemóvel", modifier = Modifier.weight(1f), fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color(0xFF101214))
+                }
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    itemsIndexed(voluntariosSim) { index, voluntario ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(if (index % 2 == 0) Color(0xFFF3F4F6) else Color(0xFFFFFFFF)),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(voluntario.nome, modifier = Modifier.weight(1f).padding(16.dp, 16.dp, 0.dp, 16.dp), fontSize = 16.sp, color = Color(0xFF101214))
+                            Text(voluntario.telemovel, modifier = Modifier.weight(1f), fontSize = 16.sp, color = Color(0xFF101214))
+                        }
+                    }
                 }
             }
         }
+
     }
 }
 
